b1eedec759ea8c14af80eff947d9e4c94cc73602
albertegi <albertegi90@gmail.com>
2026-02-09 21:19:12 +0000

    update Game with 3 attributes including: title, supportedPlatforms and coverPicture. Also added required lombok annotations

--- game-store-api/src/main/java/com/alvirg/store/game/Game.java
+++ game-store-api/src/main/java/com/alvirg/store/game/Game.java
@@ -1,4 +1,14 @@
 package com.alvirg.store.game;
 
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class Game {
+    private String title;
+    private String supportedPlatforms;
+    private String coverPicture;
 }
