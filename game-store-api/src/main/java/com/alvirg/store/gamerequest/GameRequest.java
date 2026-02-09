a6fc7603e3f183db2a44da7bc39ffc79cd8c4a5b
albertegi <albertegi90@gmail.com>
2026-02-09 21:33:08 +0000

    ** update GameRequest with title and status attributes ** update lombok annotations ** add an enum type for status

--- game-store-api/src/main/java/com/alvirg/store/gamerequest/GameRequest.java
+++ game-store-api/src/main/java/com/alvirg/store/gamerequest/GameRequest.java
@@ -1,4 +1,17 @@
 package com.alvirg.store.gamerequest;
 
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class GameRequest {
+    private String title;
+
+    @Enumerated(value = EnumType.STRING)
+    private RequestStatus status = RequestStatus.PENDING;
 }
---
+++ game-store-api/src/main/java/com/alvirg/store/gamerequest/RequestStatus.java
@@ -0,0 +1,8 @@
+package com.alvirg.store.gamerequest;
+
+public enum RequestStatus {
+    PENDING,
+    APPROVED,
+    REJECTED,
+    CANCELLED
+}
