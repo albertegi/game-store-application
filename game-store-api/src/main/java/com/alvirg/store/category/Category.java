f43be33245349353ad402d9d1933c81e1c78e176
albertegi <albertegi90@gmail.com>
2026-02-09 21:13:59 +0000

    ** update Category with two attributes: name and description ** update Category with lombok annotations

--- game-store-api/src/main/java/com/alvirg/store/category/Category.java
+++ game-store-api/src/main/java/com/alvirg/store/category/Category.java
@@ -1,4 +1,13 @@
 package com.alvirg.store.category;
 
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class Category {
+    private String name;
+    private String description;
 }
