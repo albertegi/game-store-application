a248d6669c7fa1798a30201c8559885ee8b5240f
albertegi <albertegi90@gmail.com>
2026-02-10 00:30:10 +0000

    update User class with attributes such as firstname, lastname, email and profile picture url

--- game-store-api/src/main/java/com/alvirg/store/user/User.java
+++ game-store-api/src/main/java/com/alvirg/store/user/User.java
@@ -1,4 +1,15 @@
 package com.alvirg.store.user;
 
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class User {
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String profilePictureUrl;
 }
